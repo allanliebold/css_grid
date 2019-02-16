9d0c1f6251b6b7e66d72d06926bdca54e1f089a9
Allan Liebold <allan.liebold@gmail.com>
2019-02-16 08:54:17 -0800

    Update app.js

--- project_3/js/app.js
+++ project_3/js/app.js
@@ -7,13 +7,9 @@ function eventListeners() {
   window.addEventListener('load', () => ui.hidePreloader());
   
   // toggle nav bar
-  document.querySelector('.navBtn').addEventListener('click', function() {
-    ui.showNav();
-  })
-}
+  document.querySelector('.navBtn').addEventListener('click', () => ui.showNav());
 
-function UI() {
-}
+function UI() {};
 
 UI.prototype.hidePreloader = () => document.querySelector('.preloader').style.display = 'none'; 
 UI.prototype.showNav = function() => document.querySelector('nav').classList.toggle('nav--show'); 
